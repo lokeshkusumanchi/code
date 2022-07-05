be6357ec3711d3c2ff03ef25b0366f11d4d1fed4
lokeshkusumanchi <108117802+lokeshkusumanchi@users.noreply.github.com>
2022-07-05 11:55:49 +0530

    newone
    updated

--- src/ArrayElements/Countofanelement.java
+++ src/ArrayElements/Countofanelement.java
@@ -4,7 +4,7 @@ public class Countofanelement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[]x= {1,2,3,4,3,2,5,6};
+		int[]x= {1,2,2,3,3,4,5,6,7,8,9,9};
 		int n= 3;
 		int co=0;
 		for(int i=0;i<x.length;i++) 
